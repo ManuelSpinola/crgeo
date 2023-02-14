c3a3f95048712332fbf1b745eada80630fe95a2d
ManuelSpinola <mspinola10@gmail.com>
2023-02-14 09:55:55 -0600

    spatial grids usage

--- R/cr_grids.R
+++ R/cr_grids.R
@@ -6,10 +6,16 @@
 #' cr_grid_2km
 #' cr_grid_1km
 #'
-#' @title Hierachical spatial grid system for Costa Rica
+#' @title Hierarchical spatial grid system for Costa Rica
 #'
 #' @description
 #' Spatial square grids of 8km, 4km, 2km, and 1km for Costa Rica
+#'
+#' @usage
+#' cr_grid_8km
+#' cr_grid_4km
+#' cr_grid_2km
+#' cr_grid_1km
 #'
 #' @format A sf object with a spatial grid for Costa Rica (EPSG:5367)
 #'

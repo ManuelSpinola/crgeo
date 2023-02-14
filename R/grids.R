d2ef6e39858cf345949c5063f0a17d1ee9e95345
ManuelSpinola <mspinola10@gmail.com>
2023-02-14 09:48:16 -0600

    spatial grid documentation

---
+++ R/grids.R
@@ -0,0 +1,18 @@
+#' @name cr_grid_8km
+#'
+#' @aliases
+#' cr_grid_8km
+#' cr_grid_4km
+#' cr_grid_2km
+#' cr_grid_1km
+#'
+#' @title Hierachical spatial grid system for Costa Rica
+#'
+#' @description
+#' Spatial square grids of 8km, 4km, 2km, and 1km for Costa Rica
+#'
+#' @format A sf object with a spatial grid for Costa Rica (EPSG:5367)
+#'
+#' @source The map of Costa Rica was taken from \url{https://gadm.org/index.html}
+#'
+"cr_grid_8km"
